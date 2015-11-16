70a82fb02a3a0262fad8040623ea187f7b84cc31
chansuke <moonset20@gmail.com>
2015-11-17 02:27:58 +0900

    Finish japanese correspondence

--- lib/hypertodo/task.rb
+++ lib/hypertodo/task.rb
@@ -17,6 +17,8 @@ module HyperTodo
     validates :name,    presence:     true, length: {maximum: 140}
     validates :content, presence:     true
     validates :status,  numericality: true, inclusion: {in: STATUS.values}
+    def status_name
+      STATUS.key(self.status)
+    end
   end
-
 end
